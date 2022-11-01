ad1fb640ed9e50b10e06d2059454bafb9db21f7f
Sinchana P <sinchana.p.swarna@gmail.com>
2022-11-01 13:15:46 +0530

    1st cmt

---
+++ boxes3.js
@@ -0,0 +1,87 @@
+    /**
+     * Challenge: Create state controlling whether
+     * this box is "on" or "off". Use the incoming
+     * `props.on` to determine the initial state.
+     * 
+     * Create an event listener so when the box is clicked,
+     * it toggles from "on" to "off".
+     * 
+     * Goal: clicking each box should toggle it on and off.
+     */
+
+    function Box(props) {
+            console.log(props.on)
+           
+            const [flip, setFlip] = React.useState(props.on)
+            
+            function flipColor (){
+                setFlip(oldFlip => !oldFlip)
+            }
+
+            const styles = {
+                backgroundColor: flip ? "#222222" : "transparent"
+            }
+
+          return (
+              <div className="box" style={styles} onClick = {flipColor}></div>
+          )
+        }
+        
+        
+        
+        function App(props) {
+          const boxesArray = [
+              {
+                  id: 1,
+                  on: true
+              },   
+              {
+                  id: 2,
+                  on: false
+              },   
+              {
+                  id: 3,
+                  on: true
+              },   
+              {
+                  id: 4,
+                  on: true
+              },   
+              {
+                  id: 5,
+                  on: false
+              },   
+              {
+                  id: 6,
+                  on: false
+              },  
+              {
+                  id: 7,
+                  on: true
+              },   
+              { 
+                  id: 8,
+                  on: false
+              },   
+              {
+                  id: 9,
+                  on: false
+              } 
+           ]
+        
+           const [boxes, setBoxes] = React.useState(boxesArray)
+           const displayBox = boxes.map(oldBox => (
+                    <Box key={oldBox.id} on={oldBox.on} />
+                    ))
+        
+          return (
+              <main className="container">
+                  <h1>Satisfying Boxes</h1>
+                  {displayBox}
+              </main>
+          )
+        }
+        
+        ReactDOM.render(<App darkMode={true}/>, document.getElementById("root"))
+        
+        
